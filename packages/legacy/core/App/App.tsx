6c7d2bfb1a60c673c3a0f82c9989803d194855da
gabsavard <gabsavard@credil.org>
2024-06-11 16:27:52 -0300

    feat: add new component for central connection
    - Added a new component to use the central hooks and be able to scan and
      find peripherals.
    
    Signed-off-by: gabsavard <gabsavard@credil.org>

--- packages/legacy/core/App/App.tsx
+++ packages/legacy/core/App/App.tsx
@@ -29,12 +29,16 @@ import RootStack from './navigators/RootStack'
 import { theme } from './theme'
 import {
   Central,
+  CentralProvider,
+  useCentralOnDiscovered,
+  useCentral,
   DEFAULT_DIDCOMM_SERVICE_UUID,
   DEFAULT_DIDCOMM_MESSAGE_CHARACTERISTIC_UUID,
   DEFAULT_DIDCOMM_INDICATE_CHARACTERISTIC_UUID,
 } from '@animo-id/react-native-ble-didcomm'
 //import { credentialOfferTourSteps, credentialsTourSteps, proofRequestTourSteps } from './index'
 import { BleOutboundTransport, BleInboundTransport } from '@credo-ts/transport-ble'
+import CentralDiscovery from './contexts/central_discovery'
 
 initLanguages(translationResources)
 
@@ -51,54 +55,31 @@ function App(sytem: Container) {
       SplashScreen.hide()
     }, [])
 
-    const [central, setCentral] = useState<Central | undefined>(undefined)
-
-    const startCentral = async () => {
-      const c = new Central()
-
-      await c.start()
-
-      const uuid = '56847593-40ea-4a92-bd8c-e1514dca1c61'
-      await c.setService({
-        serviceUUID: uuid || DEFAULT_DIDCOMM_SERVICE_UUID,
-        messagingUUID: DEFAULT_DIDCOMM_MESSAGE_CHARACTERISTIC_UUID,
-        indicationUUID: DEFAULT_DIDCOMM_INDICATE_CHARACTERISTIC_UUID,
-      })
-
-      setCentral(c)
-    }
-
-    const scan = async () => {
-      await central?.scan()
-      console.log('Done Scanning')
-    }
-
     return (
-      <AgentProvider agent={undefined}>
-        <ThemeProvider value={theme}>
-          <AnimatedComponentsProvider value={animatedComponents}>
-            <ConfigurationProvider value={defaultConfiguration}>
-              <AuthProvider>
-                <NetworkProvider>
-                  <TourProvider
-                    homeTourSteps={homeTourSteps}
-                    credentialsTourSteps={credentialsTourSteps}
-                    credentialOfferTourSteps={credentialOfferTourSteps}
-                    proofRequestTourSteps={proofRequestTourSteps}
-                    overlayColor={'gray'}
-                    overlayOpacity={0.7}
-                  >
-                    <Text>Hello, World!</Text>
-                    <Button title="Start Central" onPress={startCentral} />
-                    <Text>central is {central ? 'started' : 'undefined'}</Text>
-                    <Button title="Scan" onPress={scan} />
-                  </TourProvider>
-                </NetworkProvider>
-              </AuthProvider>
-            </ConfigurationProvider>
-          </AnimatedComponentsProvider>
-        </ThemeProvider>
-      </AgentProvider>
+      <CentralProvider central={new Central()}>
+        <AgentProvider agent={undefined}>
+          <ThemeProvider value={theme}>
+            <AnimatedComponentsProvider value={animatedComponents}>
+              <ConfigurationProvider value={defaultConfiguration}>
+                <AuthProvider>
+                  <NetworkProvider>
+                    <TourProvider
+                      homeTourSteps={homeTourSteps}
+                      credentialsTourSteps={credentialsTourSteps}
+                      credentialOfferTourSteps={credentialOfferTourSteps}
+                      proofRequestTourSteps={proofRequestTourSteps}
+                      overlayColor={'gray'}
+                      overlayOpacity={0.7}
+                    >
+                      <CentralDiscovery />
+                    </TourProvider>
+                  </NetworkProvider>
+                </AuthProvider>
+              </ConfigurationProvider>
+            </AnimatedComponentsProvider>
+          </ThemeProvider>
+        </AgentProvider>
+      </CentralProvider>
     )
   }
 }
---
+++ packages/legacy/core/App/contexts/central_discovery.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import { Text, Button, View } from 'react-native'
+import {
+  useCentralOnDiscovered,
+  Central,
+  DEFAULT_DIDCOMM_SERVICE_UUID,
+  DEFAULT_DIDCOMM_MESSAGE_CHARACTERISTIC_UUID,
+  DEFAULT_DIDCOMM_INDICATE_CHARACTERISTIC_UUID,
+  useCentral,
+} from '@animo-id/react-native-ble-didcomm'
+
+const CentralDiscovery = () => {
+  const { central } = useCentral()
+  const [centralState, setCentralState] = useState<Central>(central)
+
+  const startCentral = async () => {
+    const c = new Central()
+
+    await c.start()
+
+    const uuid = '56847593-40ea-4a92-bd8c-e1514dca1c61'
+    await c.setService({
+      serviceUUID: uuid || DEFAULT_DIDCOMM_SERVICE_UUID,
+      messagingUUID: DEFAULT_DIDCOMM_MESSAGE_CHARACTERISTIC_UUID,
+      indicationUUID: DEFAULT_DIDCOMM_INDICATE_CHARACTERISTIC_UUID,
+    })
+
+    setCentralState(c)
+  }
+
+  const scan = async () => {
+    await centralState?.scan()
+    console.log('Done Scanning')
+  }
+
+  const [idList, setIdList] = useState<string[]>([])
+
+  useCentralOnDiscovered((identifier) => {
+    setIdList([...idList, identifier])
+    console.log(identifier)
+    console.log('Scan')
+  })
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Button title="Start Central" onPress={startCentral} />
+      <Text>central is {central ? 'started' : 'undefined'}</Text>
+      <Button title="Scan" onPress={scan} />
+      {idList.length === 0 ? <Text>No peripherals found</Text> : idList.map((id) => <Text>{id}</Text>)}
+    </View>
+  )
+}
+
+export default CentralDiscovery
