11b2f5f452cfc135553d1f6089be4c6ba9aab14f
slacturyx <slacturyx@credil.org>
2024-06-18 11:22:37 -0400

    Update styles of peripheral screen.

--- packages/legacy/core/App/contexts/peripheral-screen.tsx
+++ packages/legacy/core/App/contexts/peripheral-screen.tsx
@@ -9,6 +9,25 @@ import { useEffect, useState } from 'react'
 import { TouchableOpacity, Text, View, StyleSheet, FlatList, Button } from 'react-native'
 import { CentralRequest, CentralRequestStatus, parseCentralMessage } from './central-screen'
 
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: '#151818',
+    flex: 1,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  btn: {
+    borderRadius: 125,
+    borderColor: '#CCF6C5',
+    width: 250,
+    height: 250,
+    borderStyle: 'solid',
+    borderWidth: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
+
 const PeripheralScreen = () => {
   const { peripheral } = usePeripheral()
   const [showCentrals, setShowCentrals] = useState(false)
@@ -38,11 +57,30 @@ const PeripheralScreen = () => {
 
   const onPress = () => console.log('Advertise')
   const renderItem = ({ identifier }: CentralRequest) => (
-    <View style={{ flex: 1, flexDirection: 'row' }}>
+    <View
+      style={{ flex: 1, alignItems: 'center', justifyContent: 'space-evenly', flexDirection: 'row', marginBottom: 20 }}
+    >
       <Text style={{ color: '#151818', fontSize: 30 }}>{identifier}</Text>
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-between', flexDirection: 'row' }}>
-        <TouchableOpacity title="Accept" onPress={() => console.log('Accept')}></TouchableOpacity>
-        <TouchableOpacity title="Reject" onPress={() => console.log('Reject')}></TouchableOpacity>
+      <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-evenly' }}>
+        <TouchableOpacity
+          style={{
+            ...styles.btn,
+            backgroundColor: '#BADFB9',
+            borderRadius: 25,
+            padding: 10,
+            width: 60,
+            height: 60,
+          }}
+          onPress={() => console.log('Accept')}
+        >
+          <Text>✔</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{ ...styles.btn, backgroundColor: '#DFC0B9', borderRadius: 25, padding: 10, width: 60, height: 60 }}
+          onPress={() => console.log('Reject')}
+        >
+          <Text>❌</Text>
+        </TouchableOpacity>
       </View>
     </View>
   )
@@ -55,41 +93,30 @@ const PeripheralScreen = () => {
         </TouchableOpacity>
       </View>
       {true && (
-        <FlatList
-          data={centralRequests}
-          renderItem={({ item }) => renderItem(item)}
+        <View
           style={{
             backgroundColor: '#CCF6C5',
             flex: 1,
             width: '100%',
             borderTopLeftRadius: 15,
             borderTopRightRadius: 15,
-            paddingHorizontal: 40,
-            paddingVertical: 10,
+            paddingHorizontal: 10,
+            paddingVertical: 5,
           }}
-        />
+        >
+          <View style={{ flex: 1, alignItems: 'flex-end' }}>
+            <TouchableOpacity
+              style={{ ...styles.btn, backgroundColor: '#BADFB9', borderRadius: 30, width: 60, height: 60 }}
+              onPress={() => setShowCentrals(false)}
+            >
+              <Text>❌</Text>
+            </TouchableOpacity>
+          </View>
+          <FlatList style={{ marginBottom: 80 }} data={centralRequests} renderItem={({ item }) => renderItem(item)} />
+        </View>
       )}
     </View>
   )
 }
 
 export default PeripheralScreen
-
-const styles = StyleSheet.create({
-  background: {
-    backgroundColor: '#151818',
-    flex: 1,
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-  },
-  btn: {
-    borderRadius: 125,
-    borderColor: '#CCF6C5',
-    width: 250,
-    height: 250,
-    borderStyle: 'solid',
-    borderWidth: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
