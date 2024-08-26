1457eaca58e92ca9549f6483a1ab0da2aa9cf699
gab savard <gabsavard@credil.org>
2024-08-26 14:11:23 -0300

    Fixed ui for central

--- packages/legacy/core/App/screens/central-screen.tsx
+++ packages/legacy/core/App/screens/central-screen.tsx
@@ -1,23 +1,33 @@
 import { useNavigation } from '@react-navigation/core'
+import { Text, TouchableOpacity, View } from 'react-native'
 import { Screens } from '../types/navigators'
 
 const CentralScreen = () => {
   const navigation = useNavigation()
 
   const onPressScanQrCodeHandler = () => {
-    navigation.getParent()?.navigate(Screens.BleScanScreen);
+    navigation.getParent()?.navigate(Screens.BleScanScreen)
   }
 
   return (
-	  <View>
-		  	<Button onPress={onScan} title="Scan with Bluetooth" />
-		    <Text>Scan list:</Text>
-		    <ScanList list={scanList} />
-			<Text>Connections List:</Text>
-			<ConnectionList list={connectionList} />
-      <Button onPress={onPressScanQrCodeHandler} title="Scan QR code" />
-	  </View>
-  );
+    <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+      <TouchableOpacity
+        style={{
+          width: 250,
+          height: 250,
+          borderWidth: 10,
+          borderRadius: 125,
+          borderColor: '#F0ACAC',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: 100,
+        }}
+        onPress={onPressScanQrCodeHandler}
+      >
+        <Text style={{ color: '#F0ACAC', fontSize: 25 }}>Scan QR Code</Text>
+      </TouchableOpacity>
+    </View>
+  )
 }
 
 export default CentralScreen
