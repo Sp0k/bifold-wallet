30c247a8204c043b1effb66cb76fb6035145e332
slacturyx <slacturyx@credil.org>
2024-08-26 15:19:20 -0400

    Refactoring the code.

--- packages/legacy/core/App/screens/peripheral-screen.tsx
+++ packages/legacy/core/App/screens/peripheral-screen.tsx
@@ -6,14 +6,12 @@ import {
   DEFAULT_DIDCOMM_INDICATE_CHARACTERISTIC_UUID,
   DEFAULT_DIDCOMM_SERVICE_UUID,
 } from '@animo-id/react-native-ble-didcomm'
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState } from 'react'
 import { BifoldError } from '../types/error'
 import {
   DeviceEventEmitter,
   Text,
-  Button,
   View,
-  FlatList,
   TouchableOpacity,
   Modal,
   TouchableWithoutFeedback,
@@ -23,20 +21,15 @@ import { Agent } from '@credo-ts/core'
 import { WalletSecret } from '../types/security'
 import { useStore } from '../contexts/store'
 import { useTranslation } from 'react-i18next'
-import { agentDependencies } from '@credo-ts/react-native'
-import BasicMessageProvider from '@credo-ts/react-hooks'
 import { BleOutboundTransport } from '@credo-ts/transport-ble'
 import { useAuth } from '../contexts/auth'
-import { didMigrateToAskar, migrateToAskar } from '../utils/migration'
-import { DispatchAction } from '../contexts/reducers/store'
-import { createLinkSecretIfRequired, getAgentModules } from '../utils/agent'
 import { useAgent } from '@credo-ts/react-hooks'
 import { useNavigation } from '@react-navigation/core'
-import { CommonActions } from '@react-navigation/core'
-import { Stacks } from '../types/navigators'
 import ConnectionList, { Connection, newConnection } from './components/ConnectionList'
 import { handlePeripheralStandardMessage } from './utils/StandardMessage'
 import QRCodeModal from '../components/modals/QRCodeModal'
+import * as InitAgent from '../utils/init_agent'
+import uuid from 'react-native-uuid';
 
 const PeripheralScreen = () => {
   const [store, dispatch] = useStore()
@@ -48,15 +41,6 @@ const PeripheralScreen = () => {
   const [connectionList, setConnectionList] = useState<Connection[]>([])
   const [modalVisible, setModalVisible] = useState<boolean>(false)
 
-  const qrCodeData = {
-    bluetooth: {
-      serviceUUID: 'c726dac1-3b2e-4bba-ad12-55bbef2cca4a',
-      messagingUUID: '24167a46-559f-4719-8c68-7f7112dd68c0',
-      indicationUUID: 'b755f21c-9f62-4d76-9eb5-6bf47fd05a8d',
-    },
-  }
-  const qrCodeValue = JSON.stringify(qrCodeData)
-
   const registerOutboundTransport = (agent: Agent, peripheral: Peripheral) => {
     const bleOutboundTransport = new BleOutboundTransport(peripheral)
 
@@ -64,88 +48,39 @@ const PeripheralScreen = () => {
   }
 
   useEffect(() => {
+    if (modalVisible === false) {
+      if (agent && agent.isInitialized) {
+        console.log("Shutdown old agent")
+        agent.shutdown();
+      }
+
+      return;
+    } 
+
+    const qrCodeData = {
+      bluetooth: {
+        serviceUUID: uuid.v4(),
+        messagingUUID: uuid.v4(),
+        indicationUUID: uuid.v4(),
+      },
+    };
+
+    setQrCodeValue(JSON.stringify(qrCodeData))
+
     const initAgentEmitError = (err: unknown) => {
       const error = new BifoldError(t('Error.Title1045'), t('Error.Message1045'), (err as Error)?.message ?? err, 1045)
       DeviceEventEmitter.emit(EventTypes.ERROR_ADDED, error)
     }
 
-    const configureAgent = (credentials: WalletSecret): Agent | undefined => {
-      try {
-        const agent = new Agent({
-          config: {
-            label: store.preferences.walletName || 'Aries Bifold',
-            walletConfig: {
-              id: credentials.id,
-              key: credentials.key ?? '',
-            },
-            autoUpdateStorageOnStartup: true,
-          },
-          dependencies: agentDependencies,
-          modules: {},
-        })
-
-        registerOutboundTransport(agent, peripheral)
-
-        return agent
-      } catch (err: unknown) {
-        initAgentEmitError(err)
-      }
+    const configureAgent = (credentials: WalletSecret | undefined): Agent | undefined => {
+      return InitAgent.configureAgent(store, credentials, undefined, [new BleOutboundTransport(peripheral)])
     }
 
     const initAgent = async (): Promise<void> => {
-      try {
-        const credentials = await getWalletCredentials()
+      const credentials = await getWalletCredentials();
+      const newAgent = configureAgent(credentials);
 
-        if (!credentials?.id || !credentials.key) {
-          // Cannot find wallet id/secret
-          return
-        }
-
-        const newAgent = configureAgent(credentials)
-
-        if (!newAgent) {
-          const error = new BifoldError(
-            t('Error.Title1045'),
-            t('Error.Message1045'),
-            'Failed to initialize agent',
-            1045
-          )
-          DeviceEventEmitter.emit(EventTypes.ERROR_ADDED, error)
-
-          return
-        }
-
-        if (!didMigrateToAskar(store.migration)) {
-          newAgent.config.logger.debug('Agent not updated to Aries Askar, updating...')
-
-          await migrateToAskar(credentials.id, credentials.key, newAgent)
-
-          newAgent.config.logger.debug('Successfully finished updating agent to Aries Askar')
-          dispatch({
-            type: DispatchAction.DID_MIGRATE_TO_ASKAR,
-          })
-        }
-
-        await newAgent.initialize()
-
-        await createLinkSecretIfRequired(newAgent)
-
-        setAgent(newAgent)
-        navigation.dispatch(
-          CommonActions.reset({
-            index: 0,
-            routes: [{ name: Stacks.TabStack }],
-          })
-        )
-      } catch (err: unknown) {
-        const error = new BifoldError(
-          t('Error.Title1045'),
-          t('Error.Message1045'),
-          (err as Error)?.message ?? err,
-          1045
-        )
-        DeviceEventEmitter.emit(EventTypes.ERROR_ADDED, error)
-      }
+      InitAgent.run(credentials, newAgent, setAgent, t);
     }
 
     const startPeripheral = async () => {
@@ -171,12 +106,12 @@ const PeripheralScreen = () => {
     initAgent()
     startPeripheral()
 
-    return () => {
-      if (agent) {
-        agent.shutdown()
-      }
-    }
-  }, [])
+    // return () => {
+    //   if (agent) {
+    //     agent.shutdown()
+    //   }
+    // }
+  }, [modalVisible])
 
   usePeripheralShutdownOnUnmount()
 
---
+++ packages/legacy/core/App/utils/init_agent.ts
@@ -0,0 +1,96 @@
+import { Agent, ConnectionsModule, InboundTransport, KeyDerivationMethod, OutboundTransport, OutOfBandApi, WalletConfig } from "@credo-ts/core";
+import { EventTypes } from "../constants";
+import { DeviceEventEmitter } from "react-native";
+import { BifoldError } from "../types/error";
+import { WalletSecret } from "../types/security";
+import { State } from "../types/state";
+import { agentDependencies } from "@credo-ts/react-native";
+import { ariesAskar } from "@hyperledger/aries-askar-react-native";
+import { AskarModule } from "@credo-ts/askar";
+
+export const registerInboundTransport = <I extends InboundTransport>(agent: Agent, inboundMessenger: I) => {
+    agent.registerInboundTransport(inboundMessenger);
+}
+
+export const rergisterOutboundTransport = <O extends OutboundTransport>(agent: Agent, outboundTransport: O) => {
+    agent.registerOutboundTransport(outboundTransport);
+}
+
+/// This is a demo, in real situation we will pass the _credentials.
+const getWalletConfig = (_credentials: WalletSecret): WalletConfig => {
+    return {
+        id: 'foo',
+        key: 'testkey000000000000000000000',
+        keyDerivationMethod: KeyDerivationMethod.Argon2IMod
+    }
+}
+
+export const configureAgent = <I extends InboundTransport, O extends OutboundTransport>(store: State, credentials: WalletSecret | undefined, inboundTransports?: I[], outboundTransports?: O[]): Agent | undefined => {
+    if (!credentials) {
+        return undefined;
+    }
+
+    try {
+        const agent = new Agent({
+            config: {
+                label: store.preferences.walletName || 'Aries Bifold',
+                walletConfig: getWalletConfig(credentials), 
+                autoUpdateStorageOnStartup: true,
+            },
+            dependencies: agentDependencies,
+            modules: {
+                connections: new ConnectionsModule({ autoAcceptConnections: true }),
+                askar: new AskarModule({
+                    ariesAskar
+                }),
+            },
+        })
+
+        for (const inboundTransport of inboundTransports ?? []) {
+            registerInboundTransport(agent, inboundTransport);
+        }
+
+        for (const outboundTransport of outboundTransports ?? []) {
+            rergisterOutboundTransport(agent, outboundTransport);
+        }
+        
+        return agent
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+export const run = async (credentials: WalletSecret | undefined, newAgent: Agent | undefined, setAgent: (agent: Agent<any>) => void, t: any): Promise<void> => {
+    try {
+        if (!credentials) {
+            return;
+        }
+
+        if (!newAgent) {
+            const error = new BifoldError(
+                t('Error.Title1045'),
+                t('Error.Message1045'),
+                'Failed to initialize agent',
+                1045
+            )
+            DeviceEventEmitter.emit(EventTypes.ERROR_ADDED, error)
+                    
+            return
+        } 
+                
+        await newAgent.initialize()
+
+        console.log("Initialized agent");
+
+        setAgent(newAgent);
+    } catch (err) {
+        const error = new BifoldError(
+            t('Error.Title1045'),
+            t('Error.Message1045'),
+            (err as Error)?.message ?? err,
+            1045,
+        )
+        DeviceEventEmitter.emit(EventTypes.ERROR_ADDED, error)
+        console.log(err);
+    }
+}
